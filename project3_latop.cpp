6e7ceedd901c4e6c084d3202fac4006a2c044977
punitjha <punitjha111@gmail.com>
2017-04-06 21:17:05 -0500

    this branch will contain all the codes

---
+++ project3_latop.cpp
@@ -0,0 +1,14 @@
+// to compile this program rum g++  project_2.cpp -01 -larmadillo llapack -lblas  -Wall 
+// the -Wall attribute is used to show all the errors you may possible get in your score code
+#include <armadillo> // the Armadillo linear algebra package of c++
+#include <iostream>
+#include <fstream>
+#include <math.h>
+#include <string>
+#include <cmath>
+#include <complex> //for using complex numbers in the program
+using namespace std;
+int main()
+{
+
+}	
