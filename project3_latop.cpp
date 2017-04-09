26ed5a6ecf700b659cca8c3ec71e4bc0aff5561e
punitjha <punit2@illinois.edu>
2017-04-09 18:46:02 -0500

    reading from a file now

--- project3_latop.cpp
+++ project3_latop.cpp
@@ -1,21 +1,34 @@
-// to compile this program run  g++  project_2.cpp -01 -larmadillo llapack -lblas  -Wall 
+// to compile this program run  g++  -o RunFileName project_2.cpp -01 -larmadillo llapack -lblas  -Wall 
 // the -Wall attribute can be used to show all the is used to show all the errors you may possible get in your score code while runnig
 #include <armadillo> // the Armadillo linear algebra package of c++
 #include <iostream>
 #include <fstream>
 #include <math.h>
-#include <string>
 #include <cmath>
+#include <string>
 #include <complex> //for using complex numbers in the program
 using namespace std;
 int main()
 {
 //building the Huckel matrix using the Armadillo Package
-	
-	arma::mat Huckel= arma::zeros(6,6);
-	arma::vec subdiagonal= arma::zeros(5);
-	subdiagonal<<-1<<-1<<-1<<-1<<-1;
-	cout<<subdiagonal;
+	string filename;
+	string line;
+	int atoms=6;
+	cout<<"Please enter the name of the molecule file"<<filename<<endl;
+	cin>>filename;
+	ifstream myfile(filename.c_str());
+	if (myfile.is_open())
+	{
+		while (getline (myfile,line))
+		{
+			cout<<line<<endl;
+		}
+	myfile.close();
+	}	
+	arma::mat Huckel= arma::zeros(atoms,atoms);
+	arma::vec subdiagonal= arma::zeros(atoms-1);
+	subdiagonal.fill(-1);
+	cout<<subdiagonal<<endl;
 	Huckel.diag(-1)=subdiagonal;
 	Huckel.diag(1)=subdiagonal;
 	Huckel(5,0)=-1;
