e0ac7796342765d6860e81401dab5d9c218ed353
punitjha <punitjha111@gmail.com>
2017-04-08 21:38:14 -0500

    Huckel for benzene working now have to go to a general program readable from a file

--- project3_latop.cpp
+++ project3_latop.cpp
@@ -1,4 +1,4 @@
-// to compile this program rum g++  project_2.cpp -01 -larmadillo llapack -lblas  -Wall 
+// to compile this program run  g++  project_2.cpp -01 -larmadillo llapack -lblas  -Wall 
 // the -Wall attribute can be used to show all the is used to show all the errors you may possible get in your score code while runnig
 #include <armadillo> // the Armadillo linear algebra package of c++
 #include <iostream>
@@ -11,7 +11,21 @@ using namespace std;
 int main()
 {
 //building the Huckel matrix using the Armadillo Package
-	arma::cx_mat HB= arma::zeros<arma::cx_mat>(4,4);
+	
+	arma::mat Huckel= arma::zeros(6,6);
+	arma::vec subdiagonal= arma::zeros(5);
+	subdiagonal<<-1<<-1<<-1<<-1<<-1;
+	cout<<subdiagonal;
+	Huckel.diag(-1)=subdiagonal;
+	Huckel.diag(1)=subdiagonal;
+	Huckel(5,0)=-1;
+	Huckel(0,5)=-1;
+	Huckel.print();
+	arma::vec eigenvalues;
+	arma::mat eigenvectors;
+	arma::eig_sym(eigenvalues, eigenvectors, Huckel);
+	eigenvalues.print();
 	complex <double> I(0.0,1.0);
-	HB(1,1)=exp(-It);
+	arma::cx_mat HB= arma::zeros<arma::cx_mat>(4,4);
+
 }	
