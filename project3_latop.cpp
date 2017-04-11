714234993dfbee63de638f290f384a6dc69ef50b
punitjha <punitjha111@gmail.com>
2017-04-11 07:32:56 -0500

    the matrix is now working-- just need to intialize the huckel now

--- project3_latop.cpp
+++ project3_latop.cpp
@@ -14,7 +14,7 @@ int main()
 //building the Huckel matrix using the Armadillo Package
 	string filename;
 	int atoms;
-	arma::mat connect(atoms,3);
+	arma::mat connect;
 //	cout<<"Please enter the name of the molecule file"<<filename<<endl;
 //	cin>>filename;
 	ifstream myfile("benzene");
@@ -24,18 +24,16 @@ int main()
 		getline(myfile,str1);
 		istringstream ee(str1);
 		ee >> atoms;
+		connect=arma::zeros(atoms,3);
 		string str;
 		int row=0;
 		while(getline(myfile,str))
 		{	
-			cout<<"this is the row"<<row<<endl;
 			istringstream ss(str);
 			int num;
 			int col=0;
 			while(ss >> num)
 			{
-				cout<<"this is the col"<<col<<endl;
-				cout<<"this is num"<<num<<endl;
 				connect(row,col)=num;
 				cout<<connect(row,col)<<endl;
 				col++;
@@ -43,31 +41,16 @@ int main()
 			row++;
 		}
 	}
-//			for (int i=0; i<atoms; i++)
-//			{	
-//				for(int j=0; j<3;j++)//the loop variable for the connectivity file is =2
-//				{
-//					myfile>>connect(i,j);
-//					cout<<"the elements of the matrix"<<i<<j<<connect(i,j)<<endl;
-//				}
-//			}
-//		}	
-	cout<<"this is the atom value"<<atoms<<endl;
 	myfile.close();
-//	connect.print();
+	connect.print();
 	arma::mat Huckel= arma::zeros(atoms,atoms);
-//	arma::vec subdiagonal= arma::zeros(atoms-1);
-//	subdiagonal.fill(-1);
-//	cout<<subdiagonal<<endl;
-//	Huckel.diag(-1)=subdiagonal;
-//	Huckel.diag(1)=subdiagonal;
-//	Huckel(5,0)=-1;
-//	Huckel(0,5)=-1;
 	for (int i=0; i<atoms; i++)
         {
 		for (int j=0;j<3;j++)
 		{
-			cout<<"this";
+			if (connect(i,j) != 0)
+			{
+				Huckel()
 		}
 	}	
 
