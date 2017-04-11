74f458de6cd43b6715e9749093a0165fa44b4c44
punitjha <punit2@illinois.edu>
2017-04-10 19:14:48 -0500

    building a generlized huckel now but uable to initialize the matrix

--- project3_latop.cpp
+++ project3_latop.cpp
@@ -6,33 +6,71 @@
 #include <math.h>
 #include <cmath>
 #include <string>
+#include <sstream>
 #include <complex> //for using complex numbers in the program
 using namespace std;
 int main()
 {
 //building the Huckel matrix using the Armadillo Package
 	string filename;
-	string line;
-	int atoms=6;
-	cout<<"Please enter the name of the molecule file"<<filename<<endl;
-	cin>>filename;
-	ifstream myfile(filename.c_str());
+	int atoms;
+	arma::mat connect(atoms,3);
+//	cout<<"Please enter the name of the molecule file"<<filename<<endl;
+//	cin>>filename;
+	ifstream myfile("benzene");
 	if (myfile.is_open())
 	{
-		while (getline (myfile,line))
-		{
-			cout<<line<<endl;
+		string str1;
+		getline(myfile,str1);
+		istringstream ee(str1);
+		ee >> atoms;
+		string str;
+		int row=0;
+		while(getline(myfile,str))
+		{	
+			cout<<"this is the row"<<row<<endl;
+			istringstream ss(str);
+			int num;
+			int col=0;
+			while(ss >> num)
+			{
+				cout<<"this is the col"<<col<<endl;
+				cout<<"this is num"<<num<<endl;
+				connect(row,col)=num;
+				cout<<connect(row,col)<<endl;
+				col++;
+			}
+			row++;
 		}
+	}
+//			for (int i=0; i<atoms; i++)
+//			{	
+//				for(int j=0; j<3;j++)//the loop variable for the connectivity file is =2
+//				{
+//					myfile>>connect(i,j);
+//					cout<<"the elements of the matrix"<<i<<j<<connect(i,j)<<endl;
+//				}
+//			}
+//		}	
+	cout<<"this is the atom value"<<atoms<<endl;
 	myfile.close();
-	}	
+//	connect.print();
 	arma::mat Huckel= arma::zeros(atoms,atoms);
-	arma::vec subdiagonal= arma::zeros(atoms-1);
-	subdiagonal.fill(-1);
-	cout<<subdiagonal<<endl;
-	Huckel.diag(-1)=subdiagonal;
-	Huckel.diag(1)=subdiagonal;
-	Huckel(5,0)=-1;
-	Huckel(0,5)=-1;
+//	arma::vec subdiagonal= arma::zeros(atoms-1);
+//	subdiagonal.fill(-1);
+//	cout<<subdiagonal<<endl;
+//	Huckel.diag(-1)=subdiagonal;
+//	Huckel.diag(1)=subdiagonal;
+//	Huckel(5,0)=-1;
+//	Huckel(0,5)=-1;
+	for (int i=0; i<atoms; i++)
+        {
+		for (int j=0;j<3;j++)
+		{
+			cout<<"this";
+		}
+	}	
+
 	Huckel.print();
 	arma::vec eigenvalues;
 	arma::mat eigenvectors;
