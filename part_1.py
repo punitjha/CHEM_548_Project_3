6f00d25dbd791216dbe6f6e1cd4a043c7f4e772a
punitjha <punit2@illinois.edu>
2017-04-13 17:19:44 -0500

    the 2nd part is now done

---
+++ part_1.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import matplotlib.pyplot as plt
+import numpy as np
+eigenvalues1=np.loadtxt('bandgap.txt')
+ee=[]
+ee1=[]
+print(len(eigenvalues1))
+x=np.linspace(-10,10,629)
+for row in range(1258):
+        if(row%2 == 0):
+            ee.append(eigenvalues1[row])
+        else:
+            ee1.append(eigenvalues1[row])   
+print(len(ee))
+plt.plot(x,ee)
+plt.plot(x,ee1)
+plt.xlabel('k -->')
+plt.show()
---
+++ part_2.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 13 14:07:19 2017
+
+@author: mintuser
+"""
+
+import matplotlib.pyplot as plt
+import numpy as np
+eigenvalues1=np.loadtxt('bandgap1.txt')
+ee=[]
+ee1=[]
+print(len(eigenvalues1))
+x=np.linspace(-10,10,629)
+for row in range(1258):
+        if(row%2 == 0):
+            ee.append(eigenvalues1[row])
+        else:
+            ee1.append(eigenvalues1[row])   
+print(len(ee))
+plt.plot(x,ee)
+plt.plot(x,ee1)
+plt.xlabel('k -->')
+plt.xlim(-10,10)
+plt.show()
