1d416b8e8a00fe40d0b2fdf6c7e06b5ca208526d
punitjha <punit2@illinois.edu>
2017-04-14 19:11:14 -0500

    add comments, k vector problem, sort the eigenvalues

---
+++ part_3.py
@@ -0,0 +1,25 @@
+ 
+
+import matplotlib.pyplot as plt
+import numpy as np
+eigenvalues1=np.loadtxt('carbon.txt')
+ee=[]
+ee1=[]
+ee2=[]
+ee3=[]
+ee=eigenvalues1[::4]
+ee1=eigenvalues1[1::4]
+ee2=eigenvalues1[2::4]
+ee3=eigenvalues1[3::4]
+x1=np.linspace(-10,10,len(ee) )
+print(len(ee))
+print(len(ee1))
+print(len(ee2))
+print(len(ee3))
+plt.plot(x1,ee)
+plt.plot(x1,ee1)
+plt.plot(x1,ee2)
+plt.plot(x1,ee3)
+plt.xlabel('k -->')
+plt.xlim(-10,10)
+plt.show()
