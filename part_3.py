b64f803ba890c8ab778095fdf779bc7386234056
punitjha <punit2@illinois.edu>
2017-04-21 19:06:44 -0500

    coming the plot commands in one file

--- part_3.py
+++ part_3.py
@@ -3,10 +3,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-
-
-
-
+from mpl_toolkits.mplot3d import Axes3D
 
 f = plt.figure(1)
 eigenvalues1=np.loadtxt('carbon.txt')
@@ -31,9 +28,43 @@ l = plt.figure(3)
 poly=np.loadtxt('poly1.txt')
 plt.plot(poly[:,0],poly[:,1],color='r', label='Total Energy')
 plt.plot(poly[:,0],poly[:,2],color='g', label='Energy')
-#plt.plot(ben[:,0],ben[:,2], color='g',label='Benzene Cation')
-#plt.xlim(-0.25,0.25)
 plt.xlabel('Delta -->')
 plt.ylabel('Energies -->')
 plt.legend()
 l.show()
+
+gg = plt.figure(4)
+eigenvalues11=np.loadtxt('carbon1.txt')
+ee=eigenvalues11[::]
+x1=np.linspace(-3.14,3.14,len(ee) )
+plt.plot(x1,ee)
+plt.xlabel('k -->')
+plt.xlim(-3.14,3.14)
+plt.show()
+gg.show()
+
+
+
+gg1 = plt.figure(5)
+k=np.loadtxt('H2D.txt')
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+x=k[:,0]
+y=k[:,1]
+z=k[:,2]
+z2=k[:,3]
+ax.plot(x, y, z, color='r')
+ax.plot(x, y, z2, color='g')
+gg1.show()
+
+
+
+
+
+
+
+
+
+
+
+
