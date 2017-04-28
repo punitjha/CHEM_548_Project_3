66ba8a3cf50a677844b85b83387617d369441235
punitjha <punit2@illinois.edu>
2017-04-28 15:56:03 -0500

    working and fully formatted

--- project3_latop.cpp
+++ project3_latop.cpp
@@ -1,11 +1,11 @@
 //**************************************************************************
-//										               *
+//									   *
 // To compile this program run:                                            *
 //g++  -o RunFileName project_2.cpp -01 -larmadillo llapack -lblas  -Wall  * 
-//									   			   *
+//									   *
 //the -Wall attribute can be used to show all the is used to show all the  *
 //errors you may possible get in your score code while runnig              *
-//                							   		   *						
+//                					   		   *						
 //**************************************************************************
 
 
@@ -22,23 +22,23 @@ int main()
 {
 
 //**************************************************************************
-//												   *								
+//									   * 
 //this part of the program construts a generalized Hamiltonian of an 	   *
 //molecule specified in the file name seciton building the Huckel          *
-//matrix using the Armadillo Package.                                      *                                                             
-//								           			   *					
+//matrix using the Armadillo Package.                                      *
+//									   *	
 //**************************************************************************
 
 
 	const double pi = 3.1415926535898;			
 	string filename;
-	int atoms;							//storage for no. of atoms
-	arma::vec k1=arma::linspace(-pi, pi,100);	//wavefactor k1
+	int atoms;						//storage for no. of atoms
+	arma::vec k1=arma::linspace(-pi, pi,100);		//wavefactor k1
 	int k1_len=k1.n_elem;					//k1 and k2 len same
-	arma::vec k2=arma::linspace(-pi, pi, 100);	//wavefactor k2
+	arma::vec k2=arma::linspace(-pi, pi, 100);		//wavefactor k2
 	complex <double> I(0.0,1.0);				//complex no. I  
 	arma::mat connect;
-	ifstream myfile("benzene"); 			//molcule connectivity file name
+	ifstream myfile("benzene"); 				//molcule connectivity file name
 	if (myfile.is_open())
 	{
 		string str1; 					//gets the first lin
@@ -46,7 +46,7 @@ int main()
 		istringstream ee(str1);				//istring - extract words from the line
 		ee >> atoms; 					//no. of atoms is assigned here
 		connect=arma::zeros(atoms,3);			//connectivity matrix-- has 3 max
-		string str;						//str for use in getline below
+		string str;					//str for use in getline below
 		int row=0;
 		while(getline(myfile,str))
 		{	
@@ -62,8 +62,8 @@ int main()
 		}
 	}
 	myfile.close();
-	arma::mat Huckel= arma::zeros(atoms,atoms); 	//Huckel matrix for TPA as semiconductor
-	arma::mat Huckel2= arma::zeros(atoms,atoms); 	//H matrix  in electron-volts.
+	arma::mat Huckel= arma::zeros(atoms,atoms);	 	//Huckel matrix for TPA as semiconductor
+	arma::mat Huckel2= arma::zeros(atoms,atoms); 		//H matrix  in electron-volts.
 	for (int i=0; i<atoms; i++)
 	{
 		int n1=connect(i,0)-1;				//extracting the elements 
@@ -94,10 +94,10 @@ int main()
 
 
 //****************************************************************************
-//												     *								 
+//									     *
 //(2-1) This part of the program is on the 1D Huckel band structure and	     *
 // applies it to the pi band of polyacetelyle showing its metallic behaviour *
-//												     *
+//									     *
 //****************************************************************************
 
 
@@ -120,10 +120,10 @@ int main()
 
 
 //*******************************************************************************
-//											         	  *								
+//										*
 //(2-2) This part of the program is on the where we have metallic behaviour of  *
 // transpolyacetelene due to equidistatn C-C bond                               *
-//													  *					
+//										*
 //*******************************************************************************
 
 
@@ -148,19 +148,19 @@ int main()
 
 
 //******************************************************************************
-//									       			 *						 
-// this part of the program calculates the band of the 4 walled carbon nanotube*                                                                  
-//									       			 *					 
+//									       *
+// this part of the program calculates the band of the 4 walled carbon nanotube*
+//									       *	
 //****************************************************************************** 
 
 	int carbon=4;							//number of carbon atoms
 	fstream myfile33("carbon.txt", fstream::out|fstream::trunc);
-	arma::cx_mat DHuckel(carbon,carbon);		 	//Hamiltonian matrix-- sum of 3 matrices
+	arma::cx_mat DHuckel(carbon,carbon);		 		//Hamiltonian matrix-- sum of 3 matrices
 	arma::cx_mat mat_1(carbon,carbon);				//first matrix in the H
 	arma::cx_mat mat_2(carbon,carbon);				//second matrix in the H--has anti-diagonal
 	arma::cx_mat mat_3(carbon,carbon);				//third  matrix in the H
 	arma::cx_vec eigencarbon;					//eigenvalues 
-	arma::cx_mat eigenvec_carbons;				//eigenvectors
+	arma::cx_mat eigenvec_carbons;					//eigenvectors
 	int diag_marker =3;						//mark anti-diagonal matrix elements
 	mat_1(carbon-1,0)=-1;						//lowermost corner element as -1
 	mat_3(0,carbon-1)=-1;						//topmost corner element as -1
@@ -188,17 +188,17 @@ int main()
 		arma::vec eigencarbon2=arma::sort(eigencarbon1);	      //sorting the real parts
 		for(int j=0; j<4;j++)
 		{
-			myfile33<<eigencarbon2(j)<<"	";		            //writing to file
+			myfile33<<eigencarbon2(j)<<"	";	            //writing to file
 		}
 		myfile33<<endl;
 	}
 
 
 //******************************************************************************
-//												       *								    
+//									       *
 // this part of the program calculates the energy bands of an wider single     *
 //walled nano tube which has 10 carbons on the circumference		       *
-//									     			       *								    
+//									       *
 //******************************************************************************
 
 
@@ -209,10 +209,10 @@ int main()
 	arma::cx_mat mat_10(carbon10,carbon10);				//first matrix in the H
 	arma::cx_mat mat_20(carbon10,carbon10);				//second matrix in the H--has a
 	arma::cx_mat mat_30(carbon10,carbon10);				//third  matrix in the H
-	arma::cx_vec eigencarbon10;						//eigenvalues 
-	arma::cx_mat eigenvec_carbons10;					//eigenvectors
-	mat_10(carbon10-1,0)=-1.0;						//lowermost corner element as -
-	mat_30(0,carbon10-1)=-1.0;						//topmost corner element as -1 	
+	arma::cx_vec eigencarbon10;					//eigenvalues 
+	arma::cx_mat eigenvec_carbons10;				//eigenvectors
+	mat_10(carbon10-1,0)=-1.0;					//lowermost corner element as -
+	mat_30(0,carbon10-1)=-1.0;					//topmost corner element as -1 	
 	mat_20(0,carbon10-1)=mat_20(carbon10-1,0)=-1.0;
 	int diag=2;
 	for(int i=0; i<10; i++)
@@ -222,7 +222,7 @@ int main()
 			mat_20(diag,i)=mat_20(i,diag)=-1.0;
 			diag=diag+2;
 		}
-		for (int j=0; j<10; j++)				      //matrix elements-- mat_1 and mat_2
+		for (int j=0; j<10; j++)			      //matrix elements-- mat_1 and mat_2
 		{
 			if((j-i) == 1)
 			{
@@ -236,24 +236,24 @@ int main()
 	}
 	for (int i=0; i<k1_len; i++)
 	{	
-		Huckel_10=mat_10*exp(-I*k1(i))+mat_20+mat_30*exp(I*k1(i));  //H matrix sum of mat_1, mat_2, mat_3
-		arma::eig_gen(eigencarbon10,eigenvec_carbons10,Huckel_10);  //eigenvalues and the eigenvectors
+		Huckel_10=mat_10*exp(-I*k1(i))+mat_20+mat_30*exp(I*k1(i));    //H matrix sum of mat_1, mat_2, mat_3
+		arma::eig_gen(eigencarbon10,eigenvec_carbons10,Huckel_10);    //eigenvalues and the eigenvectors
 		arma::vec eigencarbon1=arma::real(eigencarbon10);	      //extract real parts
 		arma::vec eigencarbon2=arma::sort(eigencarbon1);	      //sort real part 
 		for (int j=0; j<10 ; j++)
 		{
-			myfile310<<eigencarbon2(j)<<'\t';			      //eigenvalues to file 
+			myfile310<<eigencarbon2(j)<<'\t';		      //eigenvalues to file 
 		}
-		myfile310<<endl;							      //line-beaking file
+		myfile310<<endl;					      //line-beaking file
 	}
 	
 
 
 
 //*****************************************************************************
-//												      *
-// 2D-Huckel band calculation of pi band of graphene				      *								
-//									      			*							    
+//									      *
+// 2D-Huckel band calculation of pi band of graphene			      *
+//									      *	
 //*****************************************************************************
 
 	arma::cx_mat H_total(2,2);
@@ -261,7 +261,7 @@ int main()
 	arma::cx_mat H_2(2,2);					//H matrix which has -1 at (0,1)
 	arma::cx_mat H_antidiag(2,2);				//anti-diagonal matrix
 	fstream myfile333("H2D.txt", fstream::out|fstream::trunc);
-	H_1(0,0)=0;							//intializing H_1
+	H_1(0,0)=0;						//intializing H_1
 	H_1(0,1)=0;
 	H_1(1,0)=-1;
 	H_1(1,1)=0;
@@ -293,15 +293,15 @@ int main()
 	
 	int atoms_b=0;
 	arma::mat connect_b;
-	ifstream myfile44("benzene"); 			//molecule connectivity file name
+	ifstream myfile44("benzene"); 				//molecule connectivity file name
 	if (myfile44.is_open())
 	{
 		string str11; 					//gets the first lin
-		getline(myfile44,str11);				
+		getline(myfile44,str11);			
 		istringstream ee(str11);			//istring - extract words from the line
 		ee >> atoms_b; 					//no. of atoms is assigned here
-		connect_b=arma::zeros(atoms_b,3);			//connectivity matrix-- has 3 max
-		string str;						//str for use in getline below
+		connect_b=arma::zeros(atoms_b,3);		//connectivity matrix-- has 3 max
+		string str;					//str for use in getline below
 		int row=0;
 		while(getline(myfile44,str))
 		{	
@@ -310,7 +310,7 @@ int main()
 			int col=0;
 			while(ss >> num)
 			{
-				connect_b(row,col)=num; 		//assigning the connectivity matrix 
+				connect_b(row,col)=num;		//assigning the connectivity matrix 
 				col++; 				// its done column wise
 			}
 			row++;
@@ -320,9 +320,9 @@ int main()
 	arma::mat Huckel_b= arma::zeros(atoms_b,atoms_b); 	//Huckel matrix for TPA as semiconductor
 	for (int i=0; i<atoms_b; i++)
 	{
-		int n1=connect_b(i,0)-1;				//extracting the elements 
+		int n1=connect_b(i,0)-1;			//extracting the elements 
 		int n2=connect_b(i,1)-1;
-		int n3=connect_b(i,2)-1; 				//n3=0 for linear hydro-carbons
+		int n3=connect_b(i,2)-1; 			//n3=0 for linear hydro-carbons
 		Huckel_b(n1,n2)=-1;
 		Huckel_b(n2,n1)=-1;
 		if (n3 > 0) 					//if hydorcarbon has rings
@@ -333,7 +333,7 @@ int main()
 	}
 	arma::vec delta=arma::linspace(-0.25,0.25,400);
 	int delta_len=delta.n_elem;				//k1 and k2 len same
-	arma::mat eigen_b=arma::zeros(delta_len,6);	//eigenvalue for diffrent 1
+	arma::mat eigen_b=arma::zeros(delta_len,6);		//eigenvalue for diffrent 1
 	for(int i=0; i<delta_len; i++)
 	{	
 		for(int row=0; row <6; row++)
@@ -342,7 +342,7 @@ int main()
 			{
 				if (Huckel_b(row,col) !=0)
 			      {
-					if((row+col != 3) && (row+col != 9)) 			//look at H 
+					if((row+col != 3) && (row+col != 9)) 			 
 					{
 						  Huckel_b(row,col)=-1.0+delta(i);
 					}
@@ -353,7 +353,7 @@ int main()
 		}
 		arma::vec eg_b;
 		arma::mat ev_b;
-		arma::eig_sym(eg_b, ev_b, Huckel_b); 	//eigenvalues
+		arma::eig_sym(eg_b, ev_b, Huckel_b); 	
 		eigen_b.row(i)+=eg_b.t();
 	}
 	fstream myfile_11("ben_sym.txt", fstream::out|fstream::trunc);
@@ -384,13 +384,13 @@ int main()
 	for(int j=0; j<delta_1_len;j++)
 	{	
 		double sum=0.0;
-		for(int i=0; i<k1_len; i++)					//initializing -Huckel matrix
+		for(int i=0; i<k1_len; i++)					
 		{
 			Hck_p(0,0)=0.0;
 			Hck_p(0,1)=((-1.0-delta_1(j))*exp(-I*k1(i)))+(-1.0+delta_1(j));
 			Hck_p(1,0)=((-1.0-delta_1(j))*exp(I*k1(i)))+(-1.0+delta_1(j));
 			Hck_p(1,1)=0.0;
-			arma::eig_gen(ee_p,ev_p,Hck_p); 			//calculating the eigenvalues 
+			arma::eig_gen(ee_p,ev_p,Hck_p); 		//calculating the eigenvalues 
 			arma::vec eigen_ll=arma::real(ee_p);
 			sum=sum+eigen_ll(1);
 			if(i==0)
@@ -409,7 +409,7 @@ int main()
 
 
 //*******************************************************************************
-//										*
+//			BENZENE							*
 // Using the Extended Huckel theory to demonstrate the origin of the Jahn-Teller*
 // Based on Sohlber et all, J. Chem. Educ. 2013, 90 (4), 463–469.		*
 //										*	
@@ -418,8 +418,7 @@ int main()
 	const double R=(1.39/0.529);			//the bond lenght in Bohr
 	const double Z=1.56;				//orbital exponent
 	const double Z_eff=1.95;			//effective nuclear charge
-	arma::vec w=arma::linspace(0.02,0.5,30);	//the distortion parameter in b
-//	w.print();
+	arma::vec w=arma::linspace(0.0,0.5,40);		//the distortion parameter in b
 	int w_len=w.n_elem;
 	arma::mat a_val(w_len,2);
 	arma::mat b_val(w_len,2);
@@ -435,11 +434,8 @@ int main()
 			double b_ang=(2.0/3.0)*pi-a_ang;
 			x1_len=sqrt(((R-w(i))*(R-w(i)))/(2-2*cos(a_ang)));
 			x2_len=sqrt(((R+w(i))*(R+w(i)))/(2-2*cos(b_ang)));
-	//		cout<<b_ang<<" "<<a_ang<<" "<<x1_len<<" "<<x2_len<<endl;
 			if(abs(x1_len-x2_len) < 0.0001 )
 			{
-				//cout<<"this is col"<<col<<endl;
-				//cout<<a_ang<<b_ang<<x1_len<<endl;
 				if (col == 0)
 				{
 					a_val(i,col)=a_ang;
@@ -452,7 +448,6 @@ int main()
 				{
 					if (abs((x_val(i,col-1))-(x1_len))<0.00001 )
 					{
-					//	cout<<x_val(i,col-1)<<" "<<x1_len<<" "<<abs((x_val(i,col-1))-(x1_len))<<endl;
 						a_val(i,col)=a_ang;
 						b_val(i,col)=b_ang;
 						x_val(i,col)=x1_len;
@@ -463,12 +458,8 @@ int main()
 			a_ang=a_ang-0.00001;
 		}
 	}
-//	a_val.print("this is a");
-//	b_val.print("this is b"); 
-//	x_val.print("this is x");
 	arma::mat c_ang=(pi-a_val)/2;
 	arma::mat d_ang=(pi-b_val)/2;
-//	c_ang.print("this is c");
 	arma::vec r12=R-w;
 	arma::vec r23=R+w;
 	arma::vec r13=arma::zeros(w_len);
@@ -476,13 +467,11 @@ int main()
 	{
 	       	r13(i)=sqrt(r12(i)*r12(i)+r23(i)*r23(i)-(2*r12(i)*r23(i))*cos(c_ang(i,0)+d_ang(i,0)));
 	}
-//	r13.print("this is r13");
 	arma::vec r14=arma::zeros(w_len);
 	for(int i=0;i<w_len;i++)
 	{
 		r14(i)=sqrt(2*x_val(i,0)*x_val(i,0)-2*x_val(i,0)*x_val(i,0)*cos(2*a_val(i,0)+b_val(i,0)));
 	}
-//	r14.print("this is r14");
 	//setting the values of non-unique interaction distances according to symmetry
 	arma::vec r34=r12;
 	arma::vec r56=r12;
@@ -495,8 +484,6 @@ int main()
 	arma::vec r26=r13;
 	arma::vec r36=r14;
 	arma::vec r25=r14;
-//	r14.raw_print("this r14");
-//	r25.raw_print("this is r25");
 	arma::mat S=arma::zeros(6,6);
 	arma::mat Ham=arma::zeros(6,6);
 	Ham.diag().fill(-10.77/27.212);
@@ -510,7 +497,9 @@ int main()
 			Z_eff*Z_eff/r26(i)+Z_eff*Z_eff/r34(i)+Z_eff*Z_eff/r35(i)+Z_eff*Z_eff/r36(i)+
 			Z_eff*Z_eff/r45(i)+Z_eff*Z_eff/r46(i)+Z_eff*Z_eff/r56(i);
 	}
-//	Ham.print();
+	fstream ben_ee("ben_ee.txt",fstream::out | fstream::trunc);
+	fstream ben_tot("ben_tot.txt",fstream::out | fstream::trunc);
+
 	for(int i=0; i<w_len;i++)
 	{
 		S(0,2)=(1+Z*r13(i)+(2*Z*Z*r13(i)*r13(i))/5+(Z*Z*Z*r13(i)*r13(i)*r13(i))/15)*exp(-Z*r13(i));
@@ -532,10 +521,7 @@ int main()
 		arma::mat A=S.t();
 		arma::mat B=S;
 		S=A+B;
-	//	S.print("this is initial S");
 		S.diag().fill(1);
-	//	S.print(" this is S");
-	//	cout<<endl;
 	//loop to initialize the matrix elements of the Hamiltonian Marix
 		for(int row=0;row<6;row++)
 		{
@@ -543,37 +529,35 @@ int main()
 			{
 				if(row!=col)
 				{
-				//	cout<<row<<" "<<col<<"  "<<endl;
 					Ham(row,col)=0.875*S(row,col)*(Ham(row,row)+Ham(col,col));
 				}
 			}
 		}
-		//S.print("this is S");
-		//cout<<endl;
-		//Ham.print("this is H");
-		//cout<<endl;
 		arma::cx_vec ee;
 		arma::cx_mat ev;
 		arma::eig_pair(ee,ev,Ham,S);
 		arma::vec e_real=arma::real(ee);
 		e_real=arma::sort(e_real);
-		//e_real.print();
-		//cout<<endl;
+		ben_ee<<w(i)<<e_real.t();
 		Elec(i)=2*e_real(0)+2*e_real(1)+2*e_real(2);
 		Elec_cat(i)=2*e_real(0)+e_real(1)*2+e_real(2);
 		S=arma::zeros(6,6);
 	}
-	//Elec_cat.print("this is the Elec");
-	//Enuc.print("this is the Enuc");
-	arma::vec Etot=Elec_cat+Enuc;
-	//Etot.raw_print();
 
-//*******************************************************************************************************
-//
-//
-//
-//********************************************************************************************************
-	
+	arma::vec Etot=Elec_cat+Enuc;
+	for(int i=0; i<w_len; i++)
+	{
+		ben_tot<<w(i)<<" "<<Elec(i)<<" "<<Enuc(i)<<" "<<Etot(i)<<endl;
+	}
+
+//*******************************************************************************
+//			1,3,5,7-CYCLOOCTATETRAENE				*
+// Using the Extended Huckel theory to demonstrate the origin of the Jahn-Teller*
+// Based on Sohlber et all, J. Chem. Educ. 2013, 90 (4), 463–469.		*
+//										*	
+//*******************************************************************************
+
+
 	
 	
 	arma::mat a_cote(w_len,2);
@@ -590,11 +574,8 @@ int main()
 			double b_ang=(1.0/2.0)*pi-a_ang;
 			x1_len=sqrt(((R-w(i))*(R-w(i)))/(2-2*cos(a_ang)));
 			x2_len=sqrt(((R+w(i))*(R+w(i)))/(2-2*cos(b_ang)));
-	//		cout<<b_ang<<" "<<a_ang<<" "<<x1_len<<" "<<x2_len<<endl;
 			if(abs(x1_len-x2_len) < 0.0001 )
 			{
-				//cout<<"this is col"<<col<<endl;
-				//cout<<a_ang<<b_ang<<x1_len<<endl;
 				if (col == 0)
 				{
 					a_cote(i,col)=a_ang;
@@ -607,7 +588,6 @@ int main()
 				{
 					if (abs((x_cote(i,col-1))-(x1_len))<0.00001 )
 					{
-					//	cout<<x_val(i,col-1)<<" "<<x1_len<<" "<<abs((x_val(i,col-1))-(x1_len))<<endl;
 						a_cote(i,col)=a_ang;
 						b_cote(i,col)=b_ang;
 						x_cote(i,col)=x1_len;
@@ -618,12 +598,6 @@ int main()
 			a_ang=a_ang-0.00001;
 		}
 	}
-//	a_val.print("this is a");
-//	b_val.print("this is b"); 
-//	x_val.print("this is x");
-//	arma::mat c_cote=(pi-a_cote)/2;
-//	arma::mat d_cote=(pi-b_cote)/2;
-//	c_ang.print("this is c");
 	arma::vec c_r12=R-w;
 	arma::vec c_r23=R+w;
 	arma::vec c_r13=arma::zeros(w_len);
@@ -689,6 +663,8 @@ int main()
 			Z_eff*Z_eff/c_r78(i);
 
 	}
+	fstream cote_ee("cote_ee.txt",fstream::out | fstream::trunc);
+	fstream cote_tot("cote_tot.txt",fstream::out | fstream::trunc);
 	for(int i=0; i<w_len;i++)
 	{
 		S_c(0,2)=(1+Z*c_r13(i)+(2*Z*Z*c_r13(i)*c_r13(i))/5+(Z*Z*Z*c_r13(i)*c_r13(i)*c_r13(i))/15)*exp(-Z*c_r13(i));
@@ -723,10 +699,7 @@ int main()
 		arma::mat A=S_c.t();
 		arma::mat B=S_c;
 		S_c=A+B;
-	//	S.print("this is initial S");
 		S_c.diag().fill(1);
-	//	S.print(" this is S");
-	//	cout<<endl;
 	//loop to initialize the matrix elements of the Hamiltonian Marix
 		for(int row=0;row<8;row++)
 		{
@@ -734,7 +707,6 @@ int main()
 			{
 				if(row!=col)
 				{
-				//	cout<<row<<" "<<col<<"  "<<endl;
 					Ham_cote(row,col)=0.875*S_c(row,col)*(Ham_cote(row,row)+Ham_cote(col,col));
 				}
 			}
@@ -746,14 +718,15 @@ int main()
 		arma::eig_pair(eee,evv,Ham_cote,S_c);
 		arma::vec ee_real=arma::real(eee);
 		ee_real=arma::sort(ee_real);
-		ee_real;
-		cout<<endl;
 		Elec_cote(i)=2*ee_real(0)+2*ee_real(1)+2*ee_real(2)+2*ee_real(3);
+		cote_ee<<w(i)<<" "<<ee_real.t();
 		S_c=arma::zeros(8,8);
 	}
-	Elec_cote.print("this is the Elec");
-	Enuc_cote.print("this is the Enuc");
 	arma::vec Etot_cote=Elec_cote+Enuc_cote;
+	for(int i=0; i<w_len; i++)
+	{
+		cote_tot<<w(i)<<" "<<Elec_cote(i)<<" "<<Enuc_cote(i)<<" "<<Etot_cote(i)<<endl;
+	}
 	Etot_cote.raw_print();
 
 
